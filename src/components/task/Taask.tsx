fd4eac2b61c9afa505f0540d09dd1fb8c50315c5
Yury Vereskun <satriks@mail.ru>
2025-01-27 00:27:17 +0300

    add checkbox

--- src/components/task/Taask.tsx
+++ src/components/task/Taask.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TaskDTO } from "../../models/dto";
+import check from "../../assets/check.png";
 import "./Task.css";
 
 type Props = { data: TaskDTO };
@@ -7,7 +8,10 @@ type Props = { data: TaskDTO };
 export default function Task({ data }: Props) {
   return (
     <div className="task" key={data.id}>
-      <input type="radio"></input>
+      <div className="task_checkbox">
+        <div className="task_checkbox_border"></div>
+        <img className="task_checkbox_check" src={check} alt="" />
+      </div>
       <p>{data.text}</p>
     </div>
   );
